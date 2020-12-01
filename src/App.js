d3d22b4730b4ad2b6fed10f03973fc726becae79
westone034626 <dydrlaks@likelion.org>
2020-12-01 23:24:43 +0900

    test: Handle component with jsx + props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ fav }) {
+  console.log(fav);
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgyeopsal" />
+      <Food fav="jjuhhumi" />
     </div>
   );
 }
